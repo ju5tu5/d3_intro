ab2f2813107b7679191c51dc22687272888e4558
Justus Sturkenboom <justus@ju5tu5.nl>
2013-05-24 14:26:57 +0200

    After presentation, completed workshop files

---
+++ workshop_finished/oef2_script.js
@@ -0,0 +1,78 @@
+var width = 500,
+    height = 300,
+    padding = 30,
+    max = 51,
+    data = [];
+
+// Vul een array met 20 random coordinaten
+for(var i=0; i<20; i++) {
+    data.push([Math.floor(Math.random() * max), Math.floor(Math.random() * max)]);
+}
+
+// Schaalverdeling
+var xScale = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d[0]; })])
+    .range([padding, width - padding * 2]);
+
+// MAAK HIER DE SCHAALVERDELING VOOR DE Y-AS
+var yScale = d3.scale.linear()
+    .domain([0, d3.max(data, function(d) { return d[1]; })])
+    .range([height - padding, padding]);
+
+
+// Define X axis
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .ticks(5);
+
+// MAAK HIER DE Y-AS, KOPPEL DE EERDER GEMAAKTE SCHAALVERDELING
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .ticks(5);
+
+//Create SVG element
+var svg = d3.select("body")
+  .append("svg")
+    .attr({
+        width: width,
+        height: height
+    });
+
+//Create circles
+svg.selectAll("circle")
+    .data(data)
+  .enter()
+    .append("circle")
+    .attr({
+        cx: function (d) { return xScale(d[0])},
+        cy: function (d) { return yScale(d[1])},
+        r: 3,
+        class: "oef2_cirkel"
+    });
+
+//Create X axis
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (height - padding) + ")")
+    .call(xAxis);
+
+//Create Y axis
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
+
+data.push([42,42], [25, 36]); // Voeg zelf meer coordinaten toe
+var points = svg.selectAll("circle")
+    .data(data)
+    .attr("class", "oef2_cirkel_update") // Aanpassen bestaande selectie
+  .enter()
+    .append("circle") // Toevoegen nieuwe selectie
+    .attr({
+        cx: function (d) { return xScale(d[0])},
+        cy: function (d) { return yScale(d[1])},
+        r: 3,
+        class: "oef2_cirkel_enter"
+    });
